1a280cad069d3c7ba717dd9093e42b22aea0470b
Marco Liang <marcoliangbing@gmail.com>
2016-11-06 23:54:16 -0800

    add some code

---
+++ local/CommonData.java
@@ -0,0 +1,28 @@
+package local;
+
+import java.awt.geom.Point2D;
+
+import com.horstmann.violet.framework.Edge;
+import com.horstmann.violet.framework.Node;
+
+// Create by Bing Liang
+
+public class CommonData {
+	private int UserId;
+	private Node node;
+	private Edge edge;
+	private Point2D point;
+	private String command;
+	
+	// constructor for addNode
+	public CommonData(int id, Node node, Point2D point) {
+		this.UserId = id;
+		this.node = node;
+		this.point = point;
+		this.command = "addNode";
+	}
+	
+	
+	
+	
+}
---
+++ local/Console.java
@@ -0,0 +1,6 @@
+package local;
+
+// Create by Bing Liang
+public class Console {
+
+}
