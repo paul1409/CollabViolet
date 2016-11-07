48cfbe0f5487ea668616405b80a5f5a19f6b2ed2
Paul Nguyen <paul.t.nguyen@sjsu.edu>
2016-11-07 09:45:01 -0800

    Edited setup + Javadoc

--- local/CommandData.java
+++ local/CommandData.java
@@ -1,29 +1,35 @@
 package local;
 
 import java.awt.geom.Point2D;
+import java.io.Serializable;
 
 import com.horstmann.violet.framework.Edge;
 import com.horstmann.violet.framework.Node;
 
-// Create by Bing Liang
 
-public class CommandData implements serilaziable {
-	private int UserId;
-	private Node node;
-	private Edge edge;
-	private Point2D point;
-	private String command;
-	
-	// constructor for addNode
-	public CommandData(int id, Node node, Point2D point) {
-		this.UserId = id;
-		this.node = node;
-		this.point = point;
-		this.command = "addNode";
-	}
-	
-	
-	
-	
-	
+/**
+ * CommandData class
+ * @author Bing Liang
+ */
+public class CommandData implements Serializable {
+  private int UserId;
+  private Node node;
+  private Edge edge;
+  private Point2D point;
+  private String command;
+
+ 
+  /**
+   * Constructor for add node
+   * @param id An id number
+   * @param node A node
+   * @param point A point
+   */
+  public CommandData(int id, Node node, Point2D point) {
+    this.UserId = id;
+    this.node = node;
+    this.point = point;
+    this.command = "addNode";
+  }
+
 }
