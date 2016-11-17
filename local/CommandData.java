ec61222ec71e7fae176fbc68d6f1d2f4bd942358
Marco Liang <marcoliangbing@gmail.com>
2016-11-16 23:13:11 -0800

    edit Command Data

--- local/CommandData.java
+++ local/CommandData.java
@@ -2,6 +2,7 @@ package local;
 
 import java.awt.geom.Point2D;
 import java.io.Serializable;
+import java.util.LinkedList;
 
 import com.horstmann.violet.framework.Edge;
 import com.horstmann.violet.framework.Node;
@@ -13,9 +14,8 @@ import com.horstmann.violet.framework.Node;
  */
 public class CommandData implements Serializable {
   private int UserId;
-  private Node node;
-  private Edge edge;
-  private Point2D point;
+  private LinkedList<Command> commands;
+  private int size;
 
  
   /**
@@ -24,9 +24,22 @@ public class CommandData implements Serializable {
    * @param node A node
    * @param point A point
    */
-  public CommandData(int id, Node node, Point2D point) {
+  public CommandData(int id) {
     this.UserId = id;
-    this.node = node;
-    this.point = point;
-  }  
+    this.size = 0;
+  }
+  
+  public void add(Command command) {
+	  size++;
+	  commands.add(command);
+  }
+  
+  public void remove() {
+	  size--;
+	  commands.poll();
+  }
+  
+  public int size() {
+	  return size;
+  }
 }
