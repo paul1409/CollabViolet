48d6221956cd61bc1d580f421066d659a4efbd66
w851401530 <reyoungwang@gmail.com>
2016-11-16 23:37:28 -0800

    javadoc

--- cloud/Application.java
+++ cloud/Application.java
@@ -3,11 +3,20 @@ package cloud;
 import play.mvc.*;
 import java.util.ArrayList;
 import Application.Room;
+/**
+ * Class to handler everything
+ * @author reyoungwang
+ *
+ */
 public class Application extends Controller {
     private ArrayList<Room> roomList = new ArrayList<Room>();
     private int count = 0;
     
-    public Result join() {
+    /**
+     * new Room request
+     * @return
+     */
+    public Result newRoom() {
         Room e = new Room(count);
         if(!roomList.add(e))
             return badRequest();
@@ -15,9 +24,22 @@ public class Application extends Controller {
         return ok(""+count);
     }
     
+    
+    /**
+     * add action to room
+     * @param roomID the room to add
+     * @return success or failed
+     */
     public Result addAction(int roomID) {
         String command = request().body().asText();
-        request().remoteAddress();
+        Room r = roomList.get(roomID);
+        String ip = request().remoteAddress();
+        if(r.isIn(ip)) {
+            r.addInfo(command);
+            return ok();
+        } else {
+            return badRequest("Opps!");
+        }
         
     }
 
