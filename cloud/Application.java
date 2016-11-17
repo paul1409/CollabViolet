41856d604977f574a9b8b38d021ed596ba50817d
w851401530 <reyoungwang@gmail.com>
2016-11-16 23:20:17 -0800

    handler

--- cloud/Application.java
+++ cloud/Application.java
@@ -1,12 +1,24 @@
-package controllers;
+package cloud;
 
 import play.mvc.*;
-/**
- * Controller in Play Framework
- * @author reyoungwang
- *
- */
+import java.util.ArrayList;
+import Application.Room;
 public class Application extends Controller {
+    private ArrayList<Room> roomList = new ArrayList<Room>();
+    private int count = 0;
     
+    public Result join() {
+        Room e = new Room(count);
+        if(!roomList.add(e))
+            return badRequest();
+        count++;
+        return ok(""+count);
+    }
+    
+    public Result addAction(int roomID) {
+        String command = request().body().asText();
+        request().remoteAddress();
+        
+    }
 
 }
