fc7fc1488a337c38dcc1c339e180505f579ee472
w851401530 <reyoungwang@gmail.com>
2016-12-05 22:52:41 -0800

    fixbugs

--- local/SetPropertiesCommand.java
+++ local/SetPropertiesCommand.java
@@ -4,6 +4,7 @@ import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 import java.util.ArrayList;
 
+import com.horstmann.violet.framework.Edge;
 import com.horstmann.violet.framework.Graph;
 import com.horstmann.violet.framework.Node;
 
@@ -18,6 +19,8 @@ public class SetPropertiesCommand implements Command {
     private String method;
     private Class<?>[] classes;
     public SetPropertiesCommand(Object bean, Object value, String method,Class<?>[] classes) {
+        
+            System.out.println("node");
         this.classes = classes;
         this.bean = bean;
         this.value = value;
@@ -26,16 +29,26 @@ public class SetPropertiesCommand implements Command {
 
   @Override
   public void execute(Graph graph) {
+      Object beSet = null;
+      if(bean instanceof Node)  {
       ArrayList<Node> nodes = (ArrayList<Node>) (graph.getNodes());
-      Node nodeBeSet = null;
       for (Node node : nodes) {
           if (node.getID() == ((Node)bean).getID()) {
-            nodeBeSet = node;
+           beSet = node;
           }
        }
+      } else {
+          Edge cache = (Edge)bean;
+          ArrayList<Edge> edges = (ArrayList<Edge>) (graph.getEdges());
+          for (Edge e : edges) {
+              if (e.getStart().getID() == cache.getStart().getID() && e.getEnd().getID() == e.getEnd().getID()) {
+               beSet = e;
+              }
+           }
+      }
       try {
         Method m = bean.getClass().getMethod(method, classes);
-        m.invoke(nodeBeSet, new Object[] { value });
+        m.invoke(beSet, new Object[] { value });
     } catch (IllegalAccessException | IllegalArgumentException | InvocationTargetException e) {
         e.printStackTrace();
     } catch (NoSuchMethodException e) {
