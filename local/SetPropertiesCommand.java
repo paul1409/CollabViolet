f0bbe875df5c51a4dafef9fb65d4db987ccf94cd
w851401530 <reyoungwang@gmail.com>
2016-12-05 18:41:12 -0800

    completeSetPropertiesCommand

--- local/SetPropertiesCommand.java
+++ local/SetPropertiesCommand.java
@@ -1,18 +1,41 @@
 package local;
 
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.util.ArrayList;
+
 import com.horstmann.violet.framework.Graph;
+import com.horstmann.violet.framework.Node;
 
 /**
  * Sets properties
- * @author Bing Liang
+ * @author Ruiyang Wang
  *
  */
 public class SetPropertiesCommand implements Command {
+    private Object bean;
+    private Object value;
+    private Method method;
+    public SetPropertiesCommand(Object bean, Object value, Method method) {
+        this.bean = bean;
+        this.value = value;
+        this.method = method;
+    }
 
   @Override
   public void execute(Graph graph) {
-    // TODO Auto-generated method stub
-
+      ArrayList<Node> nodes = (ArrayList<Node>) (graph.getNodes());
+      Node nodeBeSet = null;
+      for (Node node : nodes) {
+          if (node.getID() == ((Node)bean).getID()) {
+            nodeBeSet = node;
+          }
+       }
+      try {
+        method.invoke(bean, new Object[] { value });
+    } catch (IllegalAccessException | IllegalArgumentException | InvocationTargetException e) {
+        e.printStackTrace();
+    }
   }
 
 }
